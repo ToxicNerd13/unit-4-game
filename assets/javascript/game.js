56736067dcc4027316b278413ef67664d4c5d2d2
ToxicNerd <k.lehenbauer13@gmail.com>
2019-09-12 13:37:33 -0700

    Got HTML and CSS all working and looking good

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// Make 4 buttons with pics of crystals
+
+// Display random number between 19-120 at beginning of each new game
+
+// Display Wins & Losses
+
+// Each crystal has its own random number value between 1-12
+
+// Display Player Score
+
+// When crystal is clicked, add its num value to Player Score
+
+// Trigger a Win if Player Score === Random Number
+
+// Trigger a Loss if Player Score > Random Number
+
+// After a Win or Loss, restart game and reassign random number value to Random Number & crystal buttons
+
+// Player Score resets to 0 
