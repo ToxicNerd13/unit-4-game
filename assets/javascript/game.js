b34c2c51a370aa0bfa6c0d30315eb2f247522531
ToxicNerd <k.lehenbauer13@gmail.com>
2019-09-12 23:02:36 -0700

    Got the wins/losses to update their html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,55 @@
-// Make 4 buttons with pics of crystals
+$(document).ready(function() {
+// Declare all the variables I will be using  in the game... These will be the initial values when new game starts
 
-// Display random number between 19-120 at beginning of each new game
+// Variable to track wins
+let wins = 0;
+// Variable to track losses
+let losses = 0;
+// Variable to track player score
+let score = 0;
+// Variable to store random goal number between 19-120
+let randomNum = numGenerator(19, 120);
+$("#random-number").text(randomNum);
 
-// Display Wins & Losses
+// Functions!!
 
-// Each crystal has its own random number value between 1-12
+// Random number generator
+function numGenerator(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+};
+// Give crystals random values
+function crystalValues() {
+    $("img").each(function() {
+        // Give the crystals in the img tag a value attribute of a random number
+        $(this).attr("value", numGenerator(1, 12))
+    });
+};
+crystalValues();
+// Start new game
+function newGame() {
+    score = 0;
+    $("#score-counter").text(score);
+    randomNum = numGenerator(19, 120);
+    crystalValues();
+}
 
-// Display Player Score
+// When user clicks a crystal image, its point value is added to the total score
+$(".crystal").click(function() {
+    score += parseInt($(this).attr("value"));
+    $("#score-counter").text(score);
 
-// When crystal is clicked, add its num value to Player Score
+    if (score === randomNum) {
+        alert("YOU WIN!");
+        wins++;
+        $("#wins").text(wins);
+        newGame();
+    }
+    else if (score > randomNum) {
+        alert("YOU LOSE");
+        losses++;
+        $("#losses").text(losses);
+        newGame();
+    }
+});
 
-// Trigger a Win if Player Score === Random Number
-
-// Trigger a Loss if Player Score > Random Number
-
-// After a Win or Loss, restart game and reassign random number value to Random Number & crystal buttons
-
-// Player Score resets to 0 
+})
